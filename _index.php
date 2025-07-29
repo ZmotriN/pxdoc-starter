0e5eac305d79ae59ea3dcd1e36c2a85edefea867
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-07-29 08:55:51 -0400

    Inital comit

---
+++ _index.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * @type     article
+ * @title    Magic The Listerine
+ * @icon     assets/images/logo-textless.svg
+ * @image    assets/images/og-image.webp
+ * @abstract Un projet de création de cartes Magic québécoises.
+ */
+?>
+ 
+
+ 
+<grostitre>Introduction</grostitre>
+
+<dots></dots>
+
+<grostitre>À propos</grostitre>
+
+<h3>Crédits</h3>
+
+<ul>
+    <li>Développeur: <em>Maxime Larrivée-Roy</em></li>
+</ul>
+
+<dots></dots>
